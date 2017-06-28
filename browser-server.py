cb1bac46dd3b9cd8aa5414e29997d89b7eb9ce85
Tom Victor <vjtomvictor@icloud.com>
2017-06-28 12:44:30 +0530

    port changed for server usage

--- browser-server.py
+++ browser-server.py
@@ -10,7 +10,7 @@ async def time(websocket, path):
         await websocket.send(now)
         await asyncio.sleep(random.random() * 3)
 
-start_server = websockets.serve(time, '127.0.0.1', 5678)
+start_server = websockets.serve(time, '0.0', 8020)
 
 asyncio.get_event_loop().run_until_complete(start_server)
 asyncio.get_event_loop().run_forever()
